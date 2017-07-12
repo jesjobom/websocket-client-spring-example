ce01bf87c7117df8bbdeb9012f90199deea38881
Jairton Junior <jairton.savicki@bbtecno.com.br>
2017-07-12 18:31:19 -0300

    starting this example

---
+++ src/main/java/com/jesjobom/websocket/WebsocketClient.java
@@ -0,0 +1,18 @@
+package com.jesjobom.websocket;
+
+/**
+ *
+ * @author jairton
+ */
+public class WebsocketClient {
+	
+	private final String url;
+
+	public WebsocketClient(String url) {
+		this.url = url;
+	}
+	
+	public void connect() {
+		
+	}
+}
